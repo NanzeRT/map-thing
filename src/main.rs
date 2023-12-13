f9df2d17fdda5e39941c6cf0026018c2f3be5837
DiedCloud <fkirill2604@gmail.com>
2023-12-13 09:18:17 +0300

    back and front are friends now

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![windows_subsystem = "windows"]
 
+use std::sync::Mutex;
 use dioxus::html::{section, style};
 use dioxus_test::header::{Header, Profile};
 use dioxus_test::asset;
 use dioxus::prelude::*;
+
+const IP: &str = "192.168.151.185";
 
 #[cfg(target_arch = "wasm32")]
 fn main() {
@@ -14,17 +17,56 @@ fn main() {
 fn main() {
     use dioxus_desktop::Config;
     let config = Config::default().with_custom_index(include_str!("web/index.html").to_string());
-    dioxus_desktop::launch_cfg(RoutePage, config);
+    dioxus_desktop::launch_cfg(MainPage, config);
+}
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
+pub struct User {
+    pub id: i32,
+    pub name: String,
+}
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
+pub struct Route {
+    pub id: i32,
+    pub name: String,
+    pub description: Option<String>,
+    pub author: User,
+    pub stars: i32,
+}
+impl Default for Route {
+    fn default() -> Self {
+        Route {
+            id: -1,
+            name: "Route name".into(),
+            description: Some("".into()),
+            author: User { id: -1, name: "Author".into()},
+            stars: 5
+        }
+    }
 }
 
 #[component]
 fn MainPage(cx: Scope) -> Element {
+    let val = use_state(cx, || String::from(""));
+
+    let fut = use_future(cx, val, |val| async move {
+        serde_json::from_str::<Vec<Route>>(
+            &reqwest::get(format!("http://{}:3000/routes?{}", IP, val))
+                .await.unwrap()
+                .text().await.unwrap()
+        )
+    });
+
+    let onsubmit = move |evt: FormEvent| {
+        val.set(evt.values["Query"][0].clone());
+    };
+
     cx.render(rsx!(
         Header {}
 
         style { include_str!("web/css/main-page-styles.css") }
         section { id: "search-row",
             form {
+                onsubmit: onsubmit,
                 input {
                     id: "search-row-field",
                     name: "Query",
@@ -35,26 +77,34 @@ fn MainPage(cx: Scope) -> Element {
             }
         }
         section { id: "route-preview-container",
-            for i in 0..10 {
-                RoutePreview {}
-                if i < 9 { rsx! {
-                    br {
-                        class: "routes-splitter"
+            match fut.value(){
+                Some(Ok(resp)) => rsx! {
+                    for i in 0..resp.len() {
+                        RoutePreview {r: resp[i].clone()}
+                        if i < resp.len()-1 { rsx! {
+                            br {
+                                class: "routes-splitter"
+                            }
+                        }}
                     }
-                }}
+                },
+                Some(Err(_)) => rsx! {
+                    RoutePreview {r: Route::default()}
+                },
+                None => rsx! {RoutePreview {r: Route::default()}},
             }
         }
     ))
 }
 
 #[component]
-fn RoutePreview(cx: Scope) -> Element {
+fn RoutePreview(cx: Scope, r: Route) -> Element {
     cx.render( rsx!(
         div { class: "route-preview",
             div { class: "route-name-and-star-container",
-                div { class: "route-name-container", h3 { class: "route-name", "Name" } }
+                div { class: "route-name-container", h3 { class: "route-name", r.name.clone() } }
                 div { class: "stars-container",
-                    for star in 1..5 {
+                    for _ in 0..r.stars {
                         img { class: "stars-img", src: asset!("/images/star.png"), alt: "star" }
                     }
                 }
@@ -89,7 +139,7 @@ fn RoutePage(cx: Scope) -> Element{
                 div { class: "tag", p { class: "tag-text", "tag" } }
             }
         }
-        div { class: "left-right-container", Profile { logged: false, size: 3.0 } }
+        div { class: "left-right-container", Profile { logged: true, size: 3.0 } }
         section { id: "description-container",
             p { id: "description",
                 r##"Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.." Многие программы электронной вёрстки и редакторы HTML используют Lorem Ipsum в качестве текста по умолчанию, так что поиск по ключевым словам "lorem ipsum" сразу показывает, как много веб-страниц всё ещё дожидаются своего настоящего рождения. За прошедшие годы текст Lorem Ipsum получил много версий. Некоторые версии появились по ошибке, некоторые - намеренно (например, юмористические варианты).
@@ -104,7 +154,7 @@ fn RoutePage(cx: Scope) -> Element{
             for _ in 0..10 {
                 div { class: "comment-container",
                     div { class: "left-right-container", style: "width: 100%",
-                        Profile { logged: false, size: 2.5 }
+                        Profile { logged: true, size: 2.5 }
                         div { class: "row-container",
                             for star in 1..2 {
                                 div { class: "row-element", img { class: "stars-img", src: asset!("/images/star.png"), alt: "star" } }
